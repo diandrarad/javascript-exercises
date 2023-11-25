fea232de07c69836efd54a0a9e8a3e5062d8c84b
unknown <diandraregita14@gmail.com>
2023-11-25 17:54:28 +0700

    Fill the functions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    let oldestPerson = null;
+    let maxAge = -1;
+  
+    const getCurrentAge = (birthYear, deathYear) => {
+      const currentYear = new Date().getFullYear();
+      const deathYearOrCurrentYear = deathYear || currentYear;
+      return deathYearOrCurrentYear - birthYear;
+    };
+  
+    for (const person of people) {
+      const { yearOfBirth, yearOfDeath } = person;
+      const currentAge = getCurrentAge(yearOfBirth, yearOfDeath);
+  
+      if (currentAge > maxAge) {
+        maxAge = currentAge;
+        oldestPerson = person;
+      }
+    }
+  
+    return oldestPerson;
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
